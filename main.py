77ba92bda49ad1f64618c9231a5064d859734a1b
Jesús Antonio Zúñiga Arce <jzuniga@uabcs.mx>
2020-06-30 18:35:33 -0600

    Add json in source file path

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
+import os
 import sys
 import xlrd
 import json
 
+def full_path(source):
+	filename = os.path.splitext(os.path.basename(source))[0]
+	fullpath =  os.path.dirname(os.path.abspath(source))
+	filenameExt = 'json'
+	return os.path.join(fullpath, filename + "." + filenameExt)
 
-if len(sys.argv) < 2:
-	raise Exception('Invalid args number')
 
-filenameFullpath = sys.argv[1]
-workbook = xlrd.open_workbook(filenameFullpath)
-worksheet = workbook.sheet_by_index(0)
-rows = worksheet.nrows
-cols = worksheet.ncols
-jsonArray = []
-for i in range(1,rows):
-	obj = {}
-	for j in range(0, cols):
-		label = worksheet.cell(0, j).value
-		obj[label] = worksheet.cell(i, j).value
-	#print json.dumps(obj)
-	jsonArray.append(obj)
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		raise Exception('Invalid args number')
 
-#print jsonArray
+	source = sys.argv[1]
+	workbook = xlrd.open_workbook(source)
+	worksheet = workbook.sheet_by_index(0)
+	rows = worksheet.nrows
+	cols = worksheet.ncols
+	jsonArray = []
+	for i in range(1,rows):
+		obj = {}
+		for j in range(0, cols):
+			label = worksheet.cell(0, j).value
+			obj[label] = worksheet.cell(i, j).value
+		jsonArray.append(obj)
 
-SEPARATOR = ''
-filename = SEPARATOR.join(filenameFullpath.split('/')[-1].split('.')[0:-1])
-print filename
 
-with open('fileParse/' + filename + '.json', 'w') as f:
-	json.dump(jsonArray, f)
 
+	fullpath = full_path(source)
+
+	with open(fullpath, 'w') as f:
+		json.dump(jsonArray, f)
